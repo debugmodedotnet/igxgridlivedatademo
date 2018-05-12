35a2903a57ca4780651177ef52acba31800346b0
debugmodedotnet <debugmode@outlook.com>
2018-05-12 16:15:53 +0530

    added more features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,10 @@ export class AppComponent implements OnInit, AfterViewInit , OnDestroy {
   //title = 'Trade Blotter CDS';
   rows: number;
   columns: number;
+  rowsCount = 1200;
   localData: BankEntity[] = [];
   localData1: BankEntity[] = [];
+  localData2: BankEntity[] = [];
   private _live = false;
   private _timer;
   persec = 10;
@@ -48,6 +50,38 @@ export class AppComponent implements OnInit, AfterViewInit , OnDestroy {
    // console.log(this.grid.verticalScrollContainer.igxForContainerSize);
   }
 
+  ngOnInit() {
+    this.appservice.getData().subscribe(t => {
+      this.localData = t;
+      this.localData2 = t;
+      this.rows = this.localData.length;
+      // this.columns = Object.keys(this.localData[0]).length;
+      this.columns = 130;
+     });
+ }
+
+  rebindgrid() {
+    this.localData = Array.from(this.localData2);
+   // this.localData = this.localData2;
+    this.localData1 = this.getMoreData(this.rowsCount-1200);
+    this.localData = this.localData.concat(this.localData1);
+    this.grid.data = this.localData;
+    this.grid.markForCheck();
+    this.rows = this.localData.length;
+  }
+
+  getMoreData(count): BankEntity[] {
+    const p = [];
+    for (let i = 0; i < count ; i ++ ) {
+        const dataindex = Math.floor(Math.random() * this.localData.length);
+        const cobject = Object.assign({}, this.localData[dataindex]);
+        cobject.TradeId = this.localData.length + i + 1;
+        p.push(cobject);
+      }
+      p.sort((a, b) =>  a.TradeId - b.TradeId );
+      return p;
+  }
+
   persecvaluechange(t) {
 
     // if (this._live) {
@@ -61,17 +95,7 @@ export class AppComponent implements OnInit, AfterViewInit , OnDestroy {
    // this._timer = setInterval(() => this.ticker(), t.value);
 
   }
-  ngOnInit() {
-     this.appservice.getData().subscribe(t => {
-       this.localData = t;
-       this.localData1 = t ;
-       this.rows = this.localData.length;
-      // console.log(t);
-       //this.columns = Object.keys(this.localData[0]).length;
-       this.columns = 130;
-      });
-     
-  }
+
 
   private ticker() {
     this.zone.runOutsideAngular(() => {
